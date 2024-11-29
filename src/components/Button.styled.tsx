e27a830594488e4ceb1714c4bb17d7d8b7c373a5
ZolotukhinIliya <1zolotyhin1@gmail.com>
2024-11-29 15:32:13 +0700

    added new files

--- src/components/Button.styled.tsx
+++ src/components/Button.styled.tsx
@@ -2,18 +2,3 @@ import styled from "styled-components";
 import {ThemeStyled} from "../styles/Theme.styled";
 import {LinkStyled} from "./Link.styled";
 
-export const ButtonStyled = styled(LinkStyled)`
-    display: flex;
-    gap: 10px;
-    align-items: center;
-    height: 58px;
-    border: none;
-    border-radius: 10px;
-    background-color: ${ThemeStyled.colors.blue};
-    padding: 10px 18px;
-
-    &:hover {
-        background-color: ${ThemeStyled.colors.navy};
-        cursor: pointer;
-    }
-`
